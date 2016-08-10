672e12aeb79be2f343476b4bed1b80ea01cae271
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-08-10 14:37:49 +0200

    hello world done and commented

--- tutorial/part_1/hello_world.asm
+++ tutorial/part_1/hello_world.asm
@@ -1,14 +1,29 @@
 section .data
-    msg db      "hello, world!"
+    msg db      "hello, world!",0x0A
 
 section .text
     global _start
 _start:
+    ;rax - temporary register; when we call a syscal, rax must contain syscall number
     mov     rax, 1
+    ;rdi - used to pass 1st argument to functions
     mov     rdi, 1
+    ;rsi - pointer used to pass 2nd argument to functions
     mov     rsi, msg
-    mov     rdx, 13
+    ;rdx - used to pass 3rd argument to functions
+    mov     rdx, 14
+    ;ssize_t sys_write(unsigned int fd, const char * buf, size_t count)
+    ;           rax        rdi               rsi             rdx
     syscall
     mov    rax, 60
     mov    rdi, 0
     syscall
+
+
+
+
+
+
+;source : http://0xax.blogspot.de/2014/08/say-hello-to-x64-assembly-part-1.html#more
+
+;vim-run: echo -e "\n\n********************";make ; ./$(basename % .asm)
